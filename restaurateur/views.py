eadfd39b5cf96cb77fe530abd31af7e0c7622e1d
pavel <paul601@mail.ru>
2021-06-09 17:05:34 +0500

    Make code less nested

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -105,17 +105,17 @@ def view_restaurants(request):
 def fetch_coordinates(apikey, place):
 
     place_data = Place.objects.filter(address=place).first()
-    if place_data is None:
-        base_url = "https://geocode-maps.yandex.ru/1.x"
-        params = {"geocode": place, "apikey": apikey, "format": "json"}
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()
-        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-        most_relevant = found_places[0]
-        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-        return Place.objects.create(address=place, lon=lon, lat=lat)
-    else:
+    if place_data is not None:
         return place_data
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    params = {"geocode": place, "apikey": apikey, "format": "json"}
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return Place.objects.create(address=place, lon=lon, lat=lat)
+
 
 
 @user_passes_test(is_manager, login_url='restaurateur:login')

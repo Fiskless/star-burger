ba1d9479f25fc7caadd9fcd658dd2035b6b92fc9
pavel <paul601@mail.ru>
2021-06-11 13:09:09 +0500

    Dry the code and make it more flexible

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -127,7 +127,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     def response_change(self, request, obj):
         res = super().response_change(request, obj)
-        if "next" in request.GET and url_has_allowed_host_and_scheme(request.GET['next'], None):
+        if request.GET.get('next') == '/manager/orders/' and \
+            url_has_allowed_host_and_scheme(request.GET['next'], None):
             return redirect(request.GET['next'])
         return res
 
